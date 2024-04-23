467433e5475ecda27f96803dd46e0917142a319b
RichardJesse <jesse.richard@strathmore.edu>
2024-04-23 20:12:05 +0300

    added an existance checker method to the database operations

--- src/Database/DatabaseOperations.java
+++ src/Database/DatabaseOperations.java
@@ -3,6 +3,7 @@ package Database;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.*;
 import java.util.Arrays;
 
 public class DatabaseOperations {
@@ -13,6 +14,13 @@ public class DatabaseOperations {
         this.queryBuilder = new QueryBuilder();
     }
 
+    /**
+     *
+     * @param table
+     * @param columns
+     * @param values
+     * @throws SQLException
+     */
     public void insertRecord(String table, String columns, String values) throws SQLException {
         Connection connection = queryBuilder.dbconnection.connection;
         connection.setAutoCommit(false);
@@ -33,6 +41,14 @@ public class DatabaseOperations {
         }
     }
 
+    /**
+     *
+     * @param table
+     * @param condition
+     * @param operator
+     * @param value
+     * @throws SQLException
+     */
     public void deleteRecord(String table, String condition, String operator, String value) throws SQLException {
         Connection connection = queryBuilder.dbconnection.connection;
         connection.setAutoCommit(false);
@@ -51,6 +67,15 @@ public class DatabaseOperations {
         }
     }
 
+    /**
+     *
+     * @param table
+     * @param updates
+     * @param condition
+     * @param operator
+     * @param conditionValue
+     * @throws SQLException
+     */
     public void updateRecord(String table, String updates, String condition, String operator, String conditionValue) throws SQLException {
         Connection connection = queryBuilder.dbconnection.connection;
         connection.setAutoCommit(false);
@@ -72,4 +97,32 @@ public class DatabaseOperations {
             connection.setAutoCommit(true);
         }
     }
+
+    public boolean checkIfRecordExist(String tableName, String columnToCheck, String valueToLookFor) throws SQLException {
+        Connection connection = queryBuilder.dbconnection.connection;
+        connection.setAutoCommit(false);
+        boolean recordExists = false;
+
+        try {
+            PreparedStatement preparedStatement = queryBuilder
+                    .select(columnToCheck)
+                    .where(columnToCheck, "=", valueToLookFor)
+                    .build();
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            connection.commit();
+
+            // Check if the ResultSet is not empty
+            recordExists = resultSet.next();
+
+        } catch (SQLException e) {
+            connection.rollback();
+            throw e;
+        } finally {
+            connection.setAutoCommit(true);
+        }
+
+        return recordExists;
+    }
+
 }
